40ec5e28ca537b803dac64026649ccc48382c566
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-05-27 09:06:09 -0300

    Initial commit

---
+++ Assets/CollisionTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+     void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "muerte")
+        {
+            //Destroy(gameObject);
+            //transform.Translate(0, 0.5F, 0);
+             
+        }
+    }
+    void OnCollisionExit()
+    {
+        
+    }
+    void OnCollisionStay()
+    {
+
+    }
+}
